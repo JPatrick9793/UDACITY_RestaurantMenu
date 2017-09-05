17b153dd24badde65bd8643c6342488fb85ae1b4
John Conway <jpatrick9793@gmail.com>
2017-09-04 21:45:47 -0400

    Changed uri: /restaurants/<int:restaurant_id>/

--- project.py
+++ project.py
@@ -1,33 +1,30 @@
 from flask import Flask
-app = Flask(__name__)
-
 # Import SQLAlchemy CRUD operations (Lesson 1)
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
 
+app = Flask(__name__)
 # Create session and connect to it
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind = engine)
 session = DBSession()
 
-@app.route('/')
-@app.route('/hello')
-def HelloWorld():
-	# return "Hello World"
-	restaurant = session.query(Restaurant).first()
-	items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
-	output = ""
-	for item in items:
-		output += item.name
-		output += '<br>'
-		output += item.price
-		output += '<br>'
-		output += item.description
-		output += '<br><br>'
-
-	return output
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+    output = ''
+    for i in items:
+        output += i.name
+        output += '</br>'
+        output += i.price
+        output += '</br>'
+        output += i.description
+        output += '</br>'
+        output += '</br>'
+    return output
 
 if __name__ == '__main__':
 	app.debug = True
